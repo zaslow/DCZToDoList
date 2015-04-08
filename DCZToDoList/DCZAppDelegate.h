091e2f3191e51bf46c932e8ff327a93fa1505762
Dean Zaslow <dcz4@pitt.edu>
2015-04-08 16:30:28 -0400

    Initial Commit

--- DCZToDoList/DCZAppDelegate.h
+++ DCZToDoList/DCZAppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface DCZAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
